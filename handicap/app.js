007d467f349bcb5a6e2d11d5a191bac41947aae4
steveoneil <steve@steveoneil.com>
2017-02-11 22:19:37 -0500

    add basic parsing and date conversion

--- handicap/app.js
+++ handicap/app.js
@@ -2,5 +2,20 @@ var xlsx = require('node-xlsx').default;
 
 const workSheetsFromFile = xlsx.parse('DataFiles/testFile.xlsx');
 
-console.log(Object.keys(workSheetsFromFile[0]['name']));
-console.log(Object.keys(workSheetsFromFile[0]['data']));
+
+function excelDateToJSDate(excelDate) {
+    return new Date((excelDate - 25568)*86400*1000).toDateString();
+}
+
+let testDate = workSheetsFromFile[0]['data'][1][3];
+console.log(testDate + "; " + excelDateToJSDate(testDate));
+
+testDate = workSheetsFromFile[0]['data'][1][8];
+console.log(testDate + "; " + excelDateToJSDate(testDate));
+
+testDate = workSheetsFromFile[0]['data'][14][8];
+console.log(testDate + "; " + excelDateToJSDate(testDate));
+
+testDate = workSheetsFromFile[0]['data'][770][3];
+console.log(testDate + "; " + excelDateToJSDate(testDate));
+
