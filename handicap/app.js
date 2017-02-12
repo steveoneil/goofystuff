49563fc64034df6ef0866c552eb9f8a2a1293185
steveoneil <steve@steveoneil.com>
2017-02-12 12:09:53 -0500

    add score posting accumulator into roster members

--- handicap/app.js
+++ handicap/app.js
@@ -3,19 +3,39 @@ var xlsx = require('node-xlsx').default;
 const workSheetsFromFile = xlsx.parse('DataFiles/testFile.xlsx');
 
 
-function excelDateToJSDate(excelDate) {
-    return new Date((excelDate - 25568)*86400*1000).toDateString();
+// function excelDateToJSDate(excelDate) {
+//     return new Date((excelDate - 25568)*86400*1000).toDateString();
+// }
+// let testDate = workSheetsFromFile[0]['data'][1][3];
+// console.log(testDate + "; " + excelDateToJSDate(testDate));
+
+function Golfer(id, name) {
+    this.id = id;
+    this.name = name;
+    this.teeSheetAppearances = 0;
+    this.scoresPosted = 0;
 }
 
-let testDate = workSheetsFromFile[0]['data'][1][3];
-console.log(testDate + "; " + excelDateToJSDate(testDate));
+const scoreColumns = workSheetsFromFile[0]['data'][0];
+const scoreData = workSheetsFromFile[0]['data'].slice(1);
 
-testDate = workSheetsFromFile[0]['data'][1][8];
-console.log(testDate + "; " + excelDateToJSDate(testDate));
+const HOMECOURSE = 'St. George\'s Golf & Country Club';
 
-testDate = workSheetsFromFile[0]['data'][14][8];
-console.log(testDate + "; " + excelDateToJSDate(testDate));
+var roster = [];
 
-testDate = workSheetsFromFile[0]['data'][770][3];
-console.log(testDate + "; " + excelDateToJSDate(testDate));
-
+for (i = 0; i < scoreData.length; i++) {
+    let scoreEntry = scoreData[i];
+    if (scoreEntry[9] === HOMECOURSE) {
+        let existingGolfer = false;
+        for (j = 0; ((j < roster.length) && (!existingGolfer)); j++) {
+            if (scoreEntry[0] === roster[j].id) {
+                roster[j].scoresPosted++;
+                existingGolfer = true;
+            }
+        }
+        if (!existingGolfer) {
+            roster.push(new Golfer(scoreEntry[0], scoreEntry[2]));
+            roster[roster.length - 1].scoresPosted++;
+        }
+    }
+}
